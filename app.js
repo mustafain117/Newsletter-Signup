85f35eaeaa13182b3042ace94456fa8f8d46763d
Mustafain Ali Khan <mustafain.khan@mail.mcgill.ca>
2020-03-03 14:38:00 -0500

    bootstrap signup added

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const bodyParser = require('body-parser')
 const request = require('request')
 
 const app = express();
+app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/signup.html')
+})
+
+
+
+
 
 app.listen('3000', function(req,res){
     console.log("Server started on port 3000");
 })
+ 
